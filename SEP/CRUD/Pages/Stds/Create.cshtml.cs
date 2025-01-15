22f7ed1264361ae2ba178377fe0b102c4ecc8892
thanhlvhe150845 <thanhlvhe150845@fpt.edu.vn>
2025-01-15 15:20:45 +0700

    update

--- SEP/CRUD/Pages/Stds/Create.cshtml.cs
+++ SEP/CRUD/Pages/Stds/Create.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using CRUD.Models;
 
+
 namespace CRUD.Pages.Stds
 {
     public class CreateModel : PageModel
@@ -20,18 +21,18 @@ namespace CRUD.Pages.Stds
 
         public IActionResult OnGet()
         {
-        ViewData["DepartId"] = new SelectList(_context.Departments, "Id", "Name");
+            ViewData["DepartId"] = new SelectList(_context.Departments, "Id", "Name");
             return Page();
         }
 
         [BindProperty]
         public Student Student { get; set; } = default!;
-        
+
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid || _context.Students == null || Student == null)
+            if (!ModelState.IsValid || _context.Students == null || Student == null)
             {
                 return Page();
             }
